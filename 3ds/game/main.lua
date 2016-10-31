345d89ddac08c718f3958d7a727b12c0dbd5fb6d
SpiralFoxes <spiraldolphin@gmail.com>
2016-10-30 21:35:50 -0400

    Add comments

--- 3ds/game/main.lua
+++ 3ds/game/main.lua
@@ -1,12 +1,17 @@
+--load everything
 function love.load()
 love.graphics.set3D(false)
 why = love.graphics.newImage('why.png')
 sound = love.audio.newSource('sad.wav')
 end
+--Draw everything
 function love.draw()
-love.graphics.setScreen('top')
-love.graphics.draw(why ,0, 0)
+-- set screen to top
+  love.graphics.setScreen('top')
+-- draw
+  love.graphics.draw(why ,0, 0)
 end
+--Quit
 function love.keypressed(key)
 if key == 'start' then
 love.event.quit()
