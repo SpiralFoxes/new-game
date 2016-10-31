ddd1ae723d2df7cdb13cfafc277fd4a8c3ba379f
SpiralFoxes <spiraldolphin@gmail.com>
2016-10-30 23:51:39 -0400

    Update main.lua

--- computer/main.lua
+++ computer/main.lua
@@ -1,8 +1,12 @@
+--load shit
 function love.load()
 why = love.graphics.newImage('why.png')
 sound = love.audio.newSource('sad.ogg')
 end
+-- draw shit
 function love.draw()
 love.graphics.draw(why ,0, 0)
+-- play music and shit
 love.audio.play(sound)
+-- end af
 end
