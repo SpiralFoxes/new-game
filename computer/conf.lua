fabfefe18b2690ec5550825d7cc1443390cd707c
SpiralFoxes <spiraldolphin@gmail.com>
2016-12-25 13:22:42 -0500

    Update conf.lua

--- computer/conf.lua
+++ computer/conf.lua
@@ -1,10 +1,7 @@
 ﻿-- Configuration
 function love.conf(t)
-	t.title = "New Game!" -- The title of the window the game is in (string)
-	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
-	t.window.width = 808        -- we want our game to be long and thin.
+	t.title = "New Game!" 
+	t.window.width = 808       
 	t.window.height = 466
-
-	-- For Windows debugging
 	t.console = false
 end
